c22b8b57784eea801e1d5e6c51c89e10bdb19b51
Mats Lundström <mats.a.lundstrom@gmail.com>
2014-10-11 15:08:25 +0200

    Remove commented code.

--- app.js
+++ app.js
@@ -1,30 +1,4 @@
-//var http, server;
-//http = require( 'http' );
-//server = http.createServer( function ( request, response ) {
-//  response.writeHead( 200, { 'Content-Type': 'text/plain' } );
-//  response.end( 'Hello World' );
-//}).listen( 3000 );
-//console.log( 'Listening on port %d', server.address().port );
-/*
-'use strict';
-var
-  http = require( 'http' ),
-  express = require( 'express' ),
-  app = express(),
-  server = http.createServer( app );
 
-
-app.get( '/', function ( request, response ) {
-  response.send( 'Hello My Express' );
-});
-
-
-server.listen( 3000 );
-console.log(
-  'Express server listening on port %d in %s mode',
-  server.address().port, app.settings.env
-);
-*/
 var express = require('express');
 var app = express();
 
